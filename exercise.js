4582207b9c6697848ede994b1ce7e40955e2c9b5
Arya Karimi <devaryakrm@gmal.com>
2022-09-17 18:54:37 +0300

    codes added

---
+++ exercise.js
@@ -0,0 +1,11 @@
+let price = 80;
+switch (price) {
+  case 50:
+    console.log("Price for Basic Room --> 50€");
+    break;
+  case 80:
+    console.log("Price for Junior Suite Room --> 80€");
+    break;
+  default:
+    console.log("Price for Master Suite --> 100€");
+}
